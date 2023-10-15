d147d4961d20726490084a80205578430372a550
Dmitry Shlepkin <acidl33t@gmail.com>
2023-10-14 22:27:06 -0400

    Initial Commit

---
+++ CompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayout
+//
+//  Created by Dmitry Shlepkin on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
